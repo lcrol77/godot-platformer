c30b3eb27ea84a4ecff403f762c965a59277acbc
Logan Croley <logan.croley.18@gmail.com>
2024-08-20 17:40:02 -0400

    feat: added pickups, better dying, audio, text, enemies

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(player: Node2D) -> void:
+	Engine.time_scale = 0.5
+	player.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,6 +18,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
+	
+	if direction > 0: 
+		animated_sprite.flip_h = false
+	elif direction < 0: 
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
